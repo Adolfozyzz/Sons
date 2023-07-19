09eb093e44ea26e7f7029169cd79363bdae4d6d5
Adolfozyzz <125566679+Adolfozyzz@users.noreply.github.com>
2023-07-18 23:56:30 -0300

    Add files via upload

---
+++ aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,46 @@
+function tocaSom (seletorAudio){
+
+   const elemento =document.querySelector(seletorAudio); 
+
+   if(elemento  && elemento.localName === 'audio'){
+   elemento.play();
+}
+else{
+    // alert('Elemento não encontrado');
+    console.log('Elemento não encontrado ou setor inválido')
+}
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++ ) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    
+    
+    tecla.onclick = function (){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento){
+        console.log(evento.code =='Space')
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+        tecla.classList.add('ativa');
+    }
+
+    
+    }
+
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
+}
+
+
+
